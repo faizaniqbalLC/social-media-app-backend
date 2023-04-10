1706ffccf24aa66e432b6ca753463fc12ffea6bb
faizaniqbalLC <se.fizaniqbal2@gmail.com>
2023-04-10 22:24:56 +0500

    added in auth > register

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,4 +3,45 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /** REGISTER USER */
-export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "Invalid Email Address!" });
+    }
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "Error Registering User.",
+    });
+  }
+};
